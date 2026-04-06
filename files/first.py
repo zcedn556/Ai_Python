ef7e728fa64bce76dad97b79de5a5cc23a9c9467
Віктор Павлюк <pavlyuvictor@gmail.com>
2026-04-06 20:53:16 +0300

    add

--- files/first.py
+++ files/first.py
@@ -19,3 +19,14 @@ df = df.sort_values(by='OrderDate', ascending=False)
 june = df[ (df['OrderDate'] >= '2023-06-05') & (df['OrderDate'] <= '2023-06-10') ]
 print(june)
 
+top3 = df.groupby('Client')["TotalAmount"].sum() \
+         .sort_values(ascending=False) \
+         .head(3)
+
+print(top3)
+
+cat = df.groupby('Category')['Quantity'].sum() 
+sales = df.groupby("Category")["TotalAmount"].sum()
+print(cat)
+print(sales)
+
