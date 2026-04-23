a7dacaead665ef1d1f2a4b3d557d20cae6353ffe
Віктор Павлюк <pavlyuvictor@gmail.com>
2026-04-23 18:17:45 +0300

    neyron1

--- files/neyron.py
+++ files/neyron.py
@@ -7,8 +7,7 @@ import pandas as pd
 
 model = keras.Sequential(
     [
-        layers.Dense(4, activation='relu', input_shape=(1,)),
-        layers.Dense(2, activation='relu'),
+        layers.Dense(8, activation='relu', input_shape=(3,)),
         layers.Dense(1)
     ]
 )
@@ -19,13 +18,14 @@ model.compile(
 )
 
 df = pd.read_csv('./csv/fuel.csv')
+df = pd.get_dummies(df, columns=['engine_type'],drop_first=True)
 
-x = df['speed_kmh']
-y = df['fuel']
+x = df[['speed_kmh', 'hour', 'engine_type_petrol']].astype(float).values
+y = df['fuel'].astype(float).values
 
-model.fit(x,y, epochs=30)
+model.fit(x,y, epochs=150)
 
-prediction = model.predict(np.array([[35], [95], [140]]))
+prediction = model.predict(np.array([[35, 12, 1]], dtype=float))
 print(prediction)
 
 model.summary()
